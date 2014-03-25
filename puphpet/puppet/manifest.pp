8634131c3d746b6f8bbd3bb721f5209247f03a27
Cyril Barragan <cyril.barragan@gmail.com>
2014-03-25 10:41:17 +0100

    Activate mod_rewrite

--- puphpet/puppet/manifest.pp
+++ puphpet/puppet/manifest.pp
@@ -878,4 +878,11 @@ package { "imagemagick":
     ensure => "installed"
 }
 
+define apache::loadmodule () {
+     exec { "/usr/sbin/a2enmod $name" :
+          unless => "/bin/readlink -e /etc/apache2/mods-enabled/${name}.load",
+          notify => Service[apache2]
+     }
+}
 
+apache::loadmodule{"rewrite": }
